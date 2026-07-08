95a0d9a45eafffd1487c96d51a70e1c15782bd24
Gabriella Martinez <gabimayern@gmail.com>
2026-07-08 16:38:06 -0700

    Create activityTracker.js

---
+++ activityTracker.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --- NOTES ---
+// map – transform each element
+[1, 2, 3].map(n => n * 2); // [2, 4, 6]
+
+// filter – keep matching elements
+[1, 2, 3, 4].filter(n => n % 2 === 0); // [2, 4]
+
+// reduce – combine into one value
+[1, 2, 3].reduce((sum, n) => sum + n, 0); // 6
